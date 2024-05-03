3885fd7aea0221a91eb69d147519b2d273daa12d
Pastafarista <antoniolandin@proton.me>
2024-05-03 20:12:54 +0200

    Custom snippet

--- nvim/lua/antonio/plugins/luasnip.lua
+++ nvim/lua/antonio/plugins/luasnip.lua
@@ -15,5 +15,24 @@ return {
 	            markdown = {"latex", "tex"}
             }),
         })
-    end,
+
+        -- Custom snippets
+        local ls = require("luasnip")
+
+        local snip = ls.snippet
+        local node = ls.snippet_node
+        local text = ls.text_node
+        local insert = ls.insert_node
+        local func = ls.function_node
+        local choice = ls.choice_node
+        local dynamicn = ls.dynamic_node
+
+        ls.add_snippets("markdown", {
+            snip("splt", {
+                text({"$$", "\\begin{split}", "\t& "}),
+                insert(1),
+                text({"\t", "\\end{split}", "$$"}),
+            })
+        })
+    end
 }
