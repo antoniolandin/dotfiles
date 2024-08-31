f0896a02b17ea5a652ade344c0f607f4d2fc7f38
Pastafarista <antoniolandin@proton.me>
2024-08-31 13:25:19 +0200

    Gitsigns se muestra por defecto

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,24 +1,22 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-            filetypes = {
-                markdown = true,
-                text = false
-            },
-            suggestion = {
-                enable = true,
-                auto_trigger = true,
-                keymap = {
-                      accept = "<S-Tab>",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+        filetypes = {
+            markdown = true,
+            text = false
+        },
+        suggestion = {
+            enable = true,
+            auto_trigger = true,
+            keymap = {
+                accept = "<S-Tab>",
                 --       accept_word = false,
                 --       accept_line = false,
-                      next = "<C-Tab>",
+                next = "<C-Tab>",
                 --       dismiss = "<M-C-Tab>",
-                }
             }
-        })
-  end,
+        }
+    }
 }
