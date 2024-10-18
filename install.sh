b6b0e30900c9a751d03cc99963f4951ccdd1fe5e
antoniolandin <antoniolandin@proton.me>
2024-10-18 17:42:06 +0200

    feat(install.sh): create home directories

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ cd ~
 rm -rf ~/yay
 
 # install dependencies
-sudo pacman -S kitty stow zsh tree-sitter-cli ripgrep fzf ttf-firacode-nerd tldr p7zip unzip go neovim python-neovim zathura zathura-pdf-mupdf git eza less python-pipx lazygit maim jq imagemagick brightnessctl
+sudo pacman -S kitty stow zsh tree-sitter-cli ripgrep fzf ttf-firacode-nerd tldr p7zip unzip go neovim python-neovim zathura zathura-pdf-mupdf git eza less python-pipx lazygit maim jq imagemagick brightnessctl python-dbus
 yay -S pulseaudio-control synology-drive
 
 # install tools with curl
@@ -36,9 +36,22 @@ stow --ignore="INSTALL.md" --dir=$HOME/Repos --target=$HOME/.config dotfiles
 # make zsh default shell
 chsh -s $(which zsh)
 
-# create histfiles
+# create directories
 mkdir $HOME/.local/state/zsh
+mkdir $HOME/Desktop $HOME/Pictures $HOME/Music $HOME/Videos $HOME/Downloads $HOME/Documents $HOME/Drive $HOME/Repos
 touch $HOME/.local/state/zsh/history
+
+# install termpdf
+mkdir ~/.virtualenvs
+cd ~/.virtualenvs
+python -m venv termpdf
+source ~/.virtualenvs/termpdf/bin/activate
+git clone https://github.com/antoniolandin/termpdf.py ~/Repos/termpdf.py
+cd ~/Repos/termpdf.py
+pip install -r requirements.txt
+chmod +x termpdf.py
+deactivate
+cd ~
 
 # now reboot shell
 echo "restart i3 and open kitty"
