56bb3b5748f5e6f587cc6871e567366090596a13
antonio <antoniolandin@proton.me>
2024-11-17 18:01:10 +0100

    fix(nvim): movement keymaps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -7,6 +7,8 @@ return {
 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+            use_default_keymaps = false,
+        })
 	end,
 }
