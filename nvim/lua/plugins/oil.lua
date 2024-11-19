05dbe9be1213f63ecae7ad11e2f8cafb292a0470
antonio <antoniolandin@proton.me>
2024-11-19 16:29:06 +0100

    change(nvim): oil keymaps

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -9,6 +9,18 @@ return {
 	config = function()
 		require("oil").setup({
             use_default_keymaps = false,
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                ["<CR>"] = "actions.select",
+                ["-"] = "actions.parent",
+                ["_"] = "actions.open_cwd",
+                ["`"] = "actions.cd",
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            }
         })
 	end,
 }
