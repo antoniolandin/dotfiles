34db757e3824ee57f0553b9f9a2fae90188d506b
antonio <antoniolandin@proton.me>
2025-03-11 13:01:59 +0100

    feat(install): added better verbose

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
+# Define colors
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+PURPLE="\033[1;35m"
+CYAN="\033[1;36m"
+GRAY='\033[90m'
+TURQUOISE="\033[1;96m"
+ORANGE="\033[38;5;208m"
+NC="\e[0m"
+
+echo -e "${RED}
+       __      __  _____ __         
+  ____/ /___  / /_/ __(_) /__  _____
+ / __  / __ \/ __/ /_/ / / _ \/ ___/
+/ /_/ / /_/ / /_/ __/ / /  __(__  ) 
+\__,_/\____/\__/_/ /_/_/\___/____/
+${NC}"
+
 # update system
+echo -e "${YELLOW}[*]${NC} Updating system"
 sudo pacman -Syu
 
 PACKAGES=(
@@ -44,15 +65,19 @@ PACKAGES=(
     "man"
 )
 
+echo -e "${YELLOW}[*]${NC} Installing multiple packages"
+
 # install packages 
 for PACKAGE_NAME in "${PACKAGES[@]}"; do
     # check if package is installed
     if ! pacman -Q "$PACKAGE_NAME" &> /dev/null; then
         sudo pacman -S --noconfirm "$PACKAGE_NAME"
     else
-        echo "$PACKAGE_NAME already installed"
+        echo -e "${GRAY}$PACKAGE_NAME already installed${NC}"
     fi
 done
+
+echo -e "${YELLOW}[*]${NC} Installing yay"
 
 # install yay
 if ! pacman -Q yay &>/dev/null; then
@@ -63,7 +88,7 @@ if ! pacman -Q yay &>/dev/null; then
     cd $HOME
     rm -rf $HOME/yay
 else
-    echo "yay already installed"
+    echo -e "${ORANGE}[*]${NC}${GRAY} yay already installed${NC}"
 fi
 
 # yay packages
@@ -71,7 +96,10 @@ YAY_PACKAGES=(
     "pulseaudio-control"
     "synology-drive"
     "librewolf-bin"
+    "fancy-cat"
 )
+
+echo -e "${YELLOW}[*]${NC} Installing multiple AUR packages"
 
 # install yay packages 
 for PACKAGE_NAME in "${YAY_PACKAGES[@]}"; do
@@ -79,12 +107,12 @@ for PACKAGE_NAME in "${YAY_PACKAGES[@]}"; do
     if ! yay -Q "$PACKAGE_NAME" &> /dev/null; then
         yay -S --noconfirm "$PACKAGE_NAME"
     else
-        echo "$PACKAGE_NAME already installed"
+        echo -e "${GRAY}$PACKAGE_NAME already installed${NC}"
     fi
 done
 
 # create librewolf desktop file
-if [! -f $HOME/.local/share/applications/librewolf.desktop ]; then
+if ! [ -f $HOME/.local/share/applications/librewolf.desktop ]; then
     echo "[Desktop Entry]
     Type=Application
     Name=LibreWolf
@@ -101,8 +129,9 @@ xdg-mime default librewolf.desktop x-scheme-handler/http
 xdg-mime default librewolf.desktop x-scheme-handler/https
 
 # install starship
+echo -e "${YELLOW}[*]${NC} Installing starship"
 if command -v starship &>/dev/null; then
-    echo "starship already installed"
+    echo -e "${ORANGE}[*]${NC}${GRAY} Starship already installed${NC}"
 else
     sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
 fi
@@ -113,23 +142,11 @@ mkdir -p $HOME/.local/state/zsh
 mkdir -p $HOME/Desktop $HOME/Videos $HOME/Downloads $HOME/Documents $HOME/Drive $HOME/Repos
 touch $HOME/.local/state/zsh/history
 
-# install termpdf
-if [ -d "$HOME/.local/share/virtualenvs/termpdf" ] && [ -f "$HOME/Repos/termpdf.py/termpdf.py" ]; then
-    echo "termpdf already installed"
-else
-    cd $HOME/.local/share/virtualenvs
-    python -m venv termpdf
-    source $HOME/.local/share/virtualenvs/termpdf/bin/activate
-    git clone https://github.com/antoniolandin/termpdf.py $HOME/Repos/termpdf.py
-    cd $HOME/Repos/termpdf.py
-    pip install -r requirements.txt
-    chmod +x termpdf.py
-    deactivate
-fi
+echo -e "${YELLOW}[*]${NC} Creating nvim virtualenv"
 
 # create neovim virtualenv
 if [ -d "$HOME/.local/share/virtualenvs/neovim" ]; then
-    echo "neovim virtualvenv already exists"
+    echo -e "${ORANGE}[*]${NC}${GRAY} Neovim virtualvenv already exists${NC}"
 else
     cd $HOME/.local/share/virtualenvs
     python -m venv neovim
@@ -142,6 +159,8 @@ function dotfiles() {
     stow --dir=$HOME/Repos --target=$HOME/.config --adopt dotfiles
     git --reset hard
 }
+
+echo -e "${YELLOW}[*]${NC} Installing dotfiles"
 
 # update dotfiles
 if [ -d "$HOME/Repos/dotfiles/.git" ]; then
@@ -156,29 +175,31 @@ if [ -d "$HOME/Repos/dotfiles/.git" ]; then
     BREF=$(git rev-parse "origin/$CURRENT_BRANCH")
     
     if [[ $AREF = $BREF ]]; then
-        echo "dotfiles are up to date"
+        echo -e "${GREEN}[*]${NC} Dotfiles are up to date"
     elif [[ $BREF == $BASE ]]; then
-        echo "dotfiles are ahead, please push local changes"
+        echo -e "${RED}[!] Dotfiles are ahead, please push local changes${NC}"
     elif [[ $AREF == $BASE ]]; then
-        echo "dotfiles are outdated, updating..."
+        echo "${YELLOW}[*]${NC} Dotfiles are outdated, updating..."
         dotfiles
     else
-        echo "dotfiles are diverged"
+        echo -e "${RED}[!] Dotfiles are diverged${NC}"
     fi
 else
 	dotfiles
 fi
 
+echo -e "${YELLOW}[*]${NC} Making zsh the default shell"
+
 # make zsh default shell
 DEFAULT_SHELL=$(basename "$SHELL")
 
 if [ "$DEFAULT_SHELL" == "zsh" ]; then
-    echo "zsh already default shell"
+    echo -e "${ORANGE}[*]${NC}${GRAY} zsh already default shell${NC}"
 else
     if command -v zsh &>/dev/null; then
         chsh -s $(which zsh)
     else
-        echo "ERROR: could not make zsh default shell, zsh is not installed"
+        echo -e "${RED}[!] Could not make zsh default shell, zsh is not installed${NC}"
     fi
 fi
 
@@ -186,33 +207,39 @@ fi
 echo 'export ZDOTDIR="$HOME/.config/zsh"' > $HOME/.profile
 source $HOME/.profile
 
-# enter zsh
-source $HOME/.config/zsh/.zshrc
+echo -e "${YELLOW}[*]${NC} Installing volta"
 
 # install volta
 if command -v volta &>/dev/null; then
-    echo "volta already installed"
+    echo -e "${ORANGE}[*]${NC}${GRAY} Volta already installed${NC}"
 else
     curl https://get.volta.sh | bash
 fi
 
+echo -e "${YELLOW}[*]${NC} Installing node"
+
 # install node
 if command -v node &>/dev/null; then
-    echo "node already installed"
+    echo -e "${ORANGE}[*]${NC}${GRAY} Node already installed${GRAY}"
 else
     volta install node
 fi
 
+echo -e "${YELLOW}[*]${NC} Installing npm"
+
 # install npm
 if command -v npm &>/dev/null; then
-    echo "npm already installed"
+    echo -e "${ORANGE}[*]${NC}${GRAY} npm already installed${NC}"
 else
     volta install npm
 fi
 
 # enable firewalld
+echo -e "${YELLOW}[*]${NC} Starting firewall"
 sudo systemctl enable firewalld.service
 sudo systemctl start firewalld.service
+
+echo -e "${GREEN}Dotfiles installation complete!${NC}"
 
 # go to home
 cd $HOME
