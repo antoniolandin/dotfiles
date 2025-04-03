c68555c1265b97dac6e10b80915b59b231746050
antonio <antoniolandin@proton.me>
2025-04-03 19:37:25 +0200

    feat(zsh): new utilities

--- zsh/utilities.zsh
+++ zsh/utilities.zsh
@@ -9,3 +9,20 @@ extractPorts () {
     bat extractPorts.tmp
     rm extractPorts.tmp
 }
+
+extractHosts () {
+    hosts=$(cat $1 | pcregrep -o1 '^Host:\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' | uniq | sort -t . -n)
+    echo $hosts > hosts.tmp
+    bat hosts.tmp
+    rm hosts.tmp
+}
+
+wlanIp () {
+    ip_mask=$(ip a | pcregrep --om-separator=" " -o1 -o2 "^\s|\t*inet\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{1,2}).+wlan0$")
+
+    ip=$(echo $ip_mask | awk '{print $1}')
+    mask=$(echo $ip_mask | awk '{print $2}')
+    
+    echo -e "\033[0;35m$ip\033[0m\033[0;34m/\033[0m\033[1;33m$mask\033[0m"
+    echo $ip | xclip -r -sel c
+}
