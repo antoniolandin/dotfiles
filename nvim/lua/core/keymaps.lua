b256e6ace1afb768aa947d0551a91463741ea7d7
antonio <antoniolandin@proton.me>
2025-06-09 10:02:02 +0200

    feat(nvim): toggle spellcheck

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,13 +2,13 @@
 vim.g.mapleader = " "
 
 --- Unbind Tab ---
-vim.keymap.set('n', '<Tab>', '<Nop>', { noremap = true, silent = true })
+vim.keymap.set("n", "<Tab>", "<Nop>", { noremap = true, silent = true })
 
 --- Unbind arrow keys ---
-for _, mode in pairs({ 'n', 'i', 'v', 'x' }) do
-    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>' }) do
-        vim.keymap.set(mode, key, '<nop>')
-    end
+for _, mode in pairs({ "n", "i", "v", "x" }) do
+	for _, key in pairs({ "<Up>", "<Down>", "<Left>", "<Right>" }) do
+		vim.keymap.set(mode, key, "<nop>")
+	end
 end
 
 --- Go to file explorer ---
@@ -38,29 +38,14 @@ vim.keymap.set("n", "<leader>oo", [[<CMD>ObsidianOpen<CR>]])
 vim.keymap.set("n", "<leader>on", [[:ObsidianNew<CR>]])
 
 --- Compiler/overseer ---
-vim.api.nvim_set_keymap('n', '<leader>co', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>cr', "<cmd>CompilerRedo<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ct', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>co", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>cr", "<cmd>CompilerRedo<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ct", "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
 
 --- Spider ---
-vim.keymap.set(
-    { "n", "o", "x" },
-    "w",
-    "<cmd>lua require('spider').motion('w')<CR>",
-    { desc = "Spider-w" }
-)
-vim.keymap.set(
-    { "n", "o", "x" },
-    "e",
-    "<cmd>lua require('spider').motion('e')<CR>",
-    { desc = "Spider-e" }
-)
-vim.keymap.set(
-    { "n", "o", "x" },
-    "b",
-    "<cmd>lua require('spider').motion('b')<CR>",
-    { desc = "Spider-b" }
-)
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
 
 --- Text Transform ---
 vim.keymap.set("n", "<leader>tc", "<cmd>TtCamel<CR>")
@@ -69,37 +54,40 @@ vim.keymap.set("n", "<leader>ts", "<cmd>TtSnake<CR>")
 --- Kitty ---
 
 -- resize window
-vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
-vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
-vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
-vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
 
 -- moving between splits
-vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
-vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
-vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
-vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
-vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+vim.keymap.set("n", "<C-\\>", require("smart-splits").move_cursor_previous)
 
 -- swapping buffers between windows
-vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
-vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
-vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
-vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
+vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
 
 -- create new splits
-vim.keymap.set('n', '<C-->', "<cmd>split<CR>")
-vim.keymap.set('n', '<C-i>', "<cmd>vsplit<CR>")
+vim.keymap.set("n", "<C-->", "<cmd>split<CR>")
+vim.keymap.set("n", "<C-i>", "<cmd>vsplit<CR>")
 
 --- Leetcode ---
-vim.keymap.set('n', "<C-'>", "<cmd>Leet test<CR>")
-vim.keymap.set('n', "<C-CR>", "<cmd>Leet submit<CR>")
+vim.keymap.set("n", "<C-'>", "<cmd>Leet test<CR>")
+vim.keymap.set("n", "<C-CR>", "<cmd>Leet submit<CR>")
 
---- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+--- Telescope ---
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 -- vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input('Grep String > ') })
+vim.keymap.set("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep String > ") })
 end, {})
+
+--- Toggle spell ---
+vim.api.nvim_set_keymap("n", "<leader>g", ":set spell!<CR>", { noremap = true, silent = true })
--- nvim/lua/core/set.lua
+++ nvim/lua/core/set.lua
@@ -1,4 +1,3 @@
-
 vim.opt.termguicolors = true
 
 vim.opt.number = true
@@ -9,64 +8,15 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
-vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 vim.opt.updatetime = 50
 vim.opt.scrolloff = 8
 vim.o.conceallevel = 1
 
 -- set python virtualenv
-vim.g.python3_host_prog=vim.fn.expand("~/.local/share/virtualenvs/neovim/bin/python3")
+vim.g.python3_host_prog = vim.fn.expand("~/.local/share/virtualenvs/neovim/bin/python3")
 
 --- set colorscheme ---
-vim.cmd('colorscheme flow')
+vim.cmd("colorscheme flow")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+vim.opt.spelllang = "es"
