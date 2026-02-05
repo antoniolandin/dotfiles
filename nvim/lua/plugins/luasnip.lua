a0815fa415eac0e45a86901e81a149121bec6d20
antonio <antoniolandin@proton.me>
2026-02-05 13:57:28 +0100

    fix(nvim): pginit snippet

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,25 +1,25 @@
 return {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
     dependencies = {
-    "rafamadriz/friendly-snippets",
-    "benfowler/telescope-luasnip.nvim",
-  },
+        "rafamadriz/friendly-snippets",
+        "benfowler/telescope-luasnip.nvim",
+    },
     config = function()
         -- Config
         require("luasnip").config.setup({
             load_ft_func = require("luasnip.extras.filetype_functions").extend_load_ft({
-	            markdown = {"latex", "tex"}
+                markdown = { "latex", "tex" }
             }),
             enable_autosnippets = true
         })
 
         -- Custom snippets
-        local ls = require("luasnip")
-
-        ls.add_snippets("markdown", require("snippets.markdown"))
+        require("luasnip.loaders.from_lua").load({
+            paths = { "~/.config/nvim/lua/snippets" }
+        })
     end
 }
---
+++ nvim/lua/snippets/python.lua
@@ -0,0 +1,48 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+return {
+    s({ trig = "pginit", dscr = "Plantilla pygame" }, {
+        t({
+            "import pygame",
+            "",
+            "LARGO, ALTO = 800, 600",
+            "BLANCO = (255, 255, 255)",
+            "FPS = 60",
+            "",
+            "pygame.init()",
+            "pantalla = pygame.display.set_mode((LARGO, ALTO))",
+            'pygame.display.set_caption("'
+        }),
+        i(1, "Nombre del proyecto"),
+        t({
+            '")',
+            "reloj = pygame.time.Clock()",
+            "",
+            "",
+            "while True:",
+            "\tfor evento in pygame.event.get():",
+            "\t\tif evento.type == pygame.QUIT:",
+            "\t\t\tpygame.quit()",
+            "\t\t\texit()",
+            "\t\telif evento.type == pygame.KEYDOWN:",
+            "\t\t\tif evento.key == pygame.K_q:",
+            "\t\t\t\tpygame.quit()",
+            "\t\t\t\texit()",
+            "",
+            "\t# RENDER",
+            "\tpantalla.fill(BLANCO)",
+            ""
+        }),
+        i(0),
+        t({
+            "",
+            "\tpygame.display.flip()",
+            "",
+            "\treloj.tick(FPS)"
+        })
+    }),
+}
