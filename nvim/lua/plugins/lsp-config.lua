62d744b21f648723e3f4d48863f901a6dbb853de
antoniolandin <antoniolandin@proton.me>
2026-05-10 22:52:44 +0200

    delete(nvim): ltex from mason ensure_installed

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,7 +1,7 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "pylsp", "ruff", "ts_ls", "lua_ls", "bashls", "clangd", "jsonls", "rust_analyzer", "perlnavigator", "texlab", "ltex" }
+        ensure_installed = { "pylsp", "ruff", "ts_ls", "lua_ls", "bashls", "clangd", "jsonls", "rust_analyzer", "perlnavigator", "texlab", }
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
